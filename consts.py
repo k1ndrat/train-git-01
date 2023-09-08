8edfb74ec02081618872c2b0268b3eee5f0ff514
k1ndrat <rubalkanaspining@gmail.com>
2023-09-08 22:27:23 +0300

    added output on end of code; added new file with constants

---
+++ consts.py
@@ -0,0 +1,1 @@
+MY_NAME = "k1ndrat"
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from consts import *
+
 print(5 + 5)
+print(MY_NAME)
 
 a = int(input("Please enter the number:"))
 
 while a > 0:
     print("Current value:", a)
     a -= 1
+
+print("Code is finished!")
